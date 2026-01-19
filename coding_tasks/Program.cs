10ee440064f551c84ba980b76fb773c8f62423bd
sergey-pavlov-it <sergey.pavlov.it@gmail.com>
2026-01-19 22:36:54 +0300

    Add project files.

---
+++ coding_tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace coding_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
