ff233e32fa385b574c101a81ae6574e7ca2ca629
sergey-pavlov-it <sergey.pavlov.it@gmail.com>
2026-01-22 23:47:51 +0300

    Вынес вопросы, ответы, диагнозы в отдельные функции. Благодаря этому уменьшилось кол-во строк кода в Main.

--- coding_tasks/Program.cs
+++ coding_tasks/Program.cs
@@ -1,38 +1,18 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace geniusIdiotConsoleApp
+﻿namespace geniusIdiotConsoleApp
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            string[] questions = new string [5];
-            questions[0] = "Сколько будет два плюс два умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
-            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-
-            int[] answers = new int [5];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
-
-            string[] diagnosis = new string [6];
-            diagnosis[0] = "Идиот";
-            diagnosis[1] = "Кретин";
-            diagnosis[2] = "Дурак";
-            diagnosis[3] = "Нормальный";
-            diagnosis[4] = "Талант";
-            diagnosis[5] = "Гений";
-
             int counterRightAnswer = 0;
+
+            string[] questions = GetArrayQuestions();
+            int[] answers = GetArrayAnswers();
+
+            RandomQuestions(questions, answers);
 
             for (int i = 0; i < 5; i++)
             {
-                RandomQuestions(questions, answers);
                 Console.WriteLine($"Вопрос №{i + 1}\n{questions[i]}");
 
                 int user_answer = int.Parse(Console.ReadLine());
@@ -43,7 +23,41 @@ namespace geniusIdiotConsoleApp
             }
 
             Console.WriteLine($"Количество верных ответов: {counterRightAnswer}");
-            Console.WriteLine($"Ваш диагноз - {diagnosis[counterRightAnswer]}");
+            Console.WriteLine($"Ваш диагноз - {Diagnosis(counterRightAnswer)}");
+        }
+
+        public static string[] GetArrayQuestions()
+        {
+            string[] questions = new string[5];
+            questions[0] = "Сколько будет два плюс два умноженное на два?";
+            questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
+            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
+            questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+            return questions;
+        }
+
+        public static int[] GetArrayAnswers()
+        {
+            int[] answers = new int[5];
+            answers[0] = 6;
+            answers[1] = 9;
+            answers[2] = 25;
+            answers[3] = 60;
+            answers[4] = 2;
+            return answers;
+        }
+
+        public static string Diagnosis(int counterRightAnswer)
+        {
+            string[] diagnosis = new string[6];
+            diagnosis[0] = "идиот";
+            diagnosis[1] = "кретин";
+            diagnosis[2] = "дурак";
+            diagnosis[3] = "нормальный";
+            diagnosis[4] = "талант";
+            diagnosis[5] = "гений";
+            return diagnosis[counterRightAnswer];
         }
 
         public static void RandomQuestions(string[] questions, int[] answers)
