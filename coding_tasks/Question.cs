455f9c9ab8dd8e3e523121b62605dba585eeaf17
sergey-pavlov-it <sergey.pavlov.it@gmail.com>
2026-01-26 22:23:22 +0300

    Добавил классы Question и QuestionStore.

---
+++ coding_tasks/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeniusIdiotConsoleApp
+{
+    public class Question
+    {
+        public string Text;
+        public int Answer;
+
+        public Question(string text, int answer) 
+        {
+            Text = text;
+            Answer = answer;
+        }
+    }
+}
---
+++ coding_tasks/QuestionStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeniusIdiotConsoleApp
+{
+    public class QuestionStore
+    {
+        public static readonly List<Question> All = new()
+        {
+            new Question("Сколько будет два плюс два умноженное на два?", 6),
+            new Question("Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?", 9),
+            new Question("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25),
+            new Question("Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60),
+            new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2),
+        };
+    }
+}
